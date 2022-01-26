d6b5aa30579ead80b4d8e57c8e4be85e5599401e
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2022-01-26 15:02:04 +0600

    Added input fields to the index.html

--- js/script.js
+++ js/script.js
@@ -1,7 +1,3 @@
-// function showMessage() {
-//     alert( 'Всем привет!' );
-// }
-
 // Функция копирования значения нажатой кнопки в
 // указанное поле ввода
 
@@ -9,21 +5,37 @@ function copyValueTo(fromElem, toElemId) {
     let elem = document.getElementById(toElemId);
     elem.value = fromElem.value;
 }
-//
-// function resetValue(elemId) {
-//     let elem = document.getElementById(elemId);
-//     elem.value = "00";
-// }
+
+// Функция сброса значений цифрового счётчика до значения 00
 
 function resetValue() {
     let i = 0;
     while (i<1) {
         let elem = document.getElementById("txHours");
-        elem.value = "00"
+        elem.value = "00";
         let elemTwo = document.getElementById("txMinutes");
-        elemTwo.value = "00"
+        elemTwo.value = "00";
         let elemThree = document.getElementById("txSeconds");
-        elemThree.value = "00"
+        elemThree.value = "00";
         i++;
     }
 }
+
+// function setupValue() {
+//     let elem = document.getElementById("txHoursTwo");
+//     let elemTwo = document.getElementById("txMinutesTwo");
+//     let elemThree = document.getElementById("txSecondsTwo");
+//
+//     if (elem.value == "00" && elemTwo.value == "00" && elemThree.value == "00") {
+//         alert('Время вышло');
+// }
+
+function inputTimeInSeconds() {
+    let hoursInSeconds = txHours.value * 3600;
+    let minutsInSeconds = txMinutes.value * 60;
+    let seconds = txSeconds.value;
+    alert(hoursInSeconds);
+    alert(minutsInSeconds);
+    alert(seconds);
+}
+
