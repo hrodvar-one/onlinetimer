0e08877fe3937f3a8feefb906ded47603794b165
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2022-01-27 09:52:01 +0600

    Added a working reverse timer

--- js/script.js
+++ js/script.js
@@ -6,20 +6,20 @@ function copyValueTo(fromElem, toElemId) {
     elem.value = fromElem.value;
 }
 
-// Функция сброса значений цифрового счётчика до значения 00
 
-function resetValue() {
-    let i = 0;
-    while (i<1) {
-        let elem = document.getElementById("txHours");
-        elem.value = "00";
-        let elemTwo = document.getElementById("txMinutes");
-        elemTwo.value = "00";
-        let elemThree = document.getElementById("txSeconds");
-        elemThree.value = "00";
-        i++;
-    }
-}
+
+// function resetValue() {
+//     let i = 0;
+//     while (i<1) {
+//         let elem = document.getElementById("txHours");
+//         elem.value = "00";
+//         let elemTwo = document.getElementById("txMinutes");
+//         elemTwo.value = "00";
+//         let elemThree = document.getElementById("txSeconds");
+//         elemThree.value = "00";
+//         i++;
+//     }
+// }
 
 // function setupValue() {
 //     let elem = document.getElementById("txHoursTwo");
@@ -30,23 +30,22 @@ function resetValue() {
 //         alert('Время вышло');
 // }
 
-let test3 = inputTimeInSeconds;
-
-setInterval(function () { timer(); }, 1000);
+// // Запускает сброс цифровых счётчиков времени после 0.5 секунд
+// setTimeout(function() { resetValue(); }, 500);
 
 // Функция перевода времени в секунды
 
-function inputTimeInSeconds() {
-    let hoursInSeconds = txHours.value * 3600;
-    let minutesInSeconds = txMinutes.value * 60;
-    let seconds = txSeconds.value * 1;
-    // alert(hoursInSeconds);
-    // alert(minutesInSeconds);
-    // alert(seconds);
-    let summTimeInSeconds = hoursInSeconds + minutesInSeconds + seconds;
-    // alert(summTimeInSeconds);
-    return summTimeInSeconds;
-}
+// function inputTimeInSeconds() {
+//     let hoursInSeconds = txHours.value * 3600;
+//     let minutesInSeconds = txMinutes.value * 60;
+//     let seconds = txSeconds.value * 1;
+//     // alert(hoursInSeconds);
+//     // alert(minutesInSeconds);
+//     // alert(seconds);
+//     let summTimeInSeconds = hoursInSeconds + minutesInSeconds + seconds;
+//     // alert(summTimeInSeconds);
+//     return summTimeInSeconds;
+// }
 
 // function timer() {
 //     while (inputTimeInSeconds() != 0) {
@@ -73,4 +72,56 @@ function timer() {
     // txHours = test / 3600;
 }
 
+// let test = document.getElementById("txSeconds");
 
+function inputTimeInSeconds() {
+    let hoursInSeconds = txHours.value * 3600;
+    let minutesInSeconds = txMinutes.value * 60;
+    let seconds = txSeconds.value * 1;
+    // alert(hoursInSeconds);
+    // alert(minutesInSeconds);
+    // alert(seconds);
+    let summTimeInSeconds = hoursInSeconds + minutesInSeconds + seconds;
+    // alert(summTimeInSeconds);
+    //вычитаю единицу из самого значения
+    summTimeInSeconds--;
+    // alert(summTimeInSeconds);
+    hoursInSeconds = Math.trunc(summTimeInSeconds / 3600);
+    minutesInSeconds = Math.trunc((summTimeInSeconds / 60) - (hoursInSeconds * 60));
+    seconds = Math.trunc(summTimeInSeconds - (minutesInSeconds * 60) - (hoursInSeconds * 3600));
+    // alert(hoursInSeconds);
+    // alert(minutesInSeconds);
+    // alert(seconds);
+    document.getElementById("txHours").value = hoursInSeconds;
+    document.getElementById("txMinutes").value = minutesInSeconds;
+    document.getElementById("txSeconds").value = seconds;
+    if (hoursInSeconds == 0 && minutesInSeconds == 0 && seconds == 0) {
+        return alert('Время вышло');
+    }
+}
+
+function startTimer () {
+    setInterval(function () { inputTimeInSeconds(); }, 1000);
+}
+
+// function startTimer () {
+//     let hours = txHours.value;
+//     let minutes = txMinutes.value;
+//     let secondsTwo = txSeconds.value;
+//     if (hours != 0 && minutes != 0 && secondsTwo !=0) {
+//         setInterval(function () { inputTimeInSeconds()}, 1000);
+//     } else {
+//         return alert('Время вышло');
+//     }
+// }
+
+// Функция сброса значений цифрового счётчика до значения 00
+
+function resetValue() {
+    let elem = document.getElementById("txHours");
+    elem.value = "00";
+    let elemTwo = document.getElementById("txMinutes");
+    elemTwo.value = "00";
+    let elemThree = document.getElementById("txSeconds");
+    elemThree.value = "00";
+}
